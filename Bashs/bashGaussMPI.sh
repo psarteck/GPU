b54111a0f3f2695959d6a7d0ad543f909093708a
UCaromel <caromelulysse@gmail.com>
2024-01-06 19:20:44 +0100

    re bash mpi

--- Bashs/bashGaussMPI.sh
+++ Bashs/bashGaussMPI.sh
@@ -1,29 +1,31 @@
-$src_folder = "..\MPI"
-$results_folder = "..\Results"
-$exe_folder = "..\Executables"
-$python_folder = "..\CodesPython"
+#!/bin/bash
 
-$nprocess = (Get-Content ..\parametres_Machine | Select-String -Pattern "Nb_Procs").Split('=')[1].Trim()
+src_folder="../MPI"
+results_folder="../Results"
+exe_folder="../Executables"
+python_folder="../CodesPython"
 
-Remove-Item $results_folder\gauss_MPI* -Force
+nprocess=$(awk -F'=' '/Nb_Procs/{print $2}' ../parametres_Machine | tr -d ' ')
 
-if ($env:OSTYPE -like "darwin*") {
-    mpic++ -I /opt/homebrew/include/eigen3 -Xlinker -debug_snapshot $src_folder\gauss2DMPI.cpp -o $exe_folder\gauss2DMPI.exe
-} else {
-    Write-Output "Pas de solution de compilation sur Windows / Linux avec Eigen"
-}
+rm $results_folder/gauss_MPI*
 
-if ($?) {
-    for ($process = 1; $process -le $nprocess; $process++) {
-        $i = 1024
-        Write-Output "Calcul sur $process processeur(s)"
-        while ($i -le 8400) {
-            mpiexec -np $process $exe_folder\gauss2DMPI.exe $i
-            $i *= 2
-        }
-    }
-    python3 $python_folder\graph.py "gauss_MPI"
-}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    mpic++ -I /opt/homebrew/include/eigen3 -Xlinker -debug_snapshot $src_folder/gauss2DMPI.cpp -o $exe_folder/gauss2DMPI.out
+else
+    echo "Pas de solution de compilation sur Windows / Linux avec Eigen"
+fi
+
+if [ $? -eq 0 ]; then
+    for ((process=1; process<=$nprocess; process++)); do
+        i=1024
+        echo "Calcul sur $process processeur(s)"
+        while [ $i -le 8400 ]; do
+            mpirun -np $process $exe_folder/gauss2DMPI.out $i 
+            i=$((i * 2)) 
+        done 
+    done
+    python3 $python_folder/graph.py "gauss_MPI"
+fi
 
 
 
